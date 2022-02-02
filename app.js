011afa65341a3bbb5738da08e6cc793d50b582aa
Your Name <you@example.com>
2022-02-02 19:45:59 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,164 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const ping = require('net-ping');
+const config = require('./config.json');
+const { exec } = require('child_process');
+const fs = require('fs');
+
+
+if (config.hosts == undefined) {
+    config.hosts = [];
+}
+if (config.graphs == undefined) {
+    config.graphs = [];
+}
+if (isNaN(config.pingInterval)) {
+    config.pingInterval = 5000;
+}
+
+const upStates = new Array(config.hosts.length);
+
+const pingOpts = {
+    retries: 1,
+    timeout: 2000
+};
+const session = ping.createSession (pingOpts);
+
+const logOpts = {
+    timestampFormat: 'YYYY-MM-DD HH:mm:ss',
+    logDirectory: __dirname + '/logs',
+    fileNamePattern:'ping-<DATE>.log',
+    dateFormat:'YYYY-MM'
+};
+const log = require('simple-node-logger').createRollingFileLogger( logOpts );
+log.warn('Server restarted');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
+function loadLogs() {
+    const date = new Date();
+    
+    var logsLastMonth = [];
+    var logsThisMonth = [];
+    var thisMonth = date.getFullYear()+'-'+padTo2Digits(date.getMonth() + 1);
+    var lastMonth = date.getFullYear()+'-'+padTo2Digits(date.getMonth());
+    if(date.getMonth() == 0){
+        lastMonth = date.getFullYear()-1+'-12';
+    } 
+    
+    try {
+        logsLastMonth = fs.readFileSync(__dirname + '/logs/ping-'+lastMonth+'.log').toString().split("\n");
+        logsLastMonth.splice(-1);
+        logsLastMonth.reverse();
+        } catch (err) {
+    }
+
+    try {
+        logsThisMonth = fs.readFileSync(__dirname + '/logs/ping-'+thisMonth+'.log').toString().split("\n");
+        logsThisMonth.splice(-1);
+        logsThisMonth.reverse();
+        } catch (err) {
+    }
+    
+    const logs = logsThisMonth.concat(logsLastMonth);
+    
+    return logs;
+}
+
+
+
+function getTimestamp() {
+    const date = new Date();
+    return (
+        [
+            date.getFullYear(),
+            padTo2Digits(date.getMonth() + 1),
+            padTo2Digits(date.getDate()),
+        ].join('-') +
+        ' ' +
+        [
+            padTo2Digits(date.getHours()),
+            padTo2Digits(date.getMinutes()),
+            padTo2Digits(date.getSeconds()),
+        ].join(':')
+    );
+}
+
+function stateChange(state, index, target, info) {
+    if( upStates[index] !== undefined) {
+        if (upStates[index] != state) {
+            upStates[index] = state;
+            log.info(target+' '+info);
+            io.emit('message', getTimestamp() + ' INFO  ' + target+' '+info);
+        }
+    } else {
+        upStates[index] = state;
+    }
+}
+
+
+io.on('connection', (socket) => {
+    socket.emit('downstreamConfig', config);
+    
+    socket.on('upstreamConfig', (newConfig) => {
+        fs.writeFile(__dirname + '/config.json', newConfig, err => {
+            if (err) {
+                console.log('Error writing new config file:', err);
+            } else {exec('pm2 restart projectPing', (err, stdout, stderr) => {
+                  if (err) {
+                    console.error(err);
+                  }
+                });
+            }
+          });
+    });
+
+    //socket.on('serverReconnect', () => {
+    //    socket.emit('message', getTimestamp() + ' WARN  ' +'Server restarted');
+    //});
+});
+
+
+setInterval(function(){
+    for (var i = 0; i < config.hosts.length; i++) {
+        session.pingHost (config.hosts[i].targetAddress, function (error, target, sent, rcvd) {
+        
+            const index = config.hosts.findIndex(x => x.targetAddress === target);
+            const ms = rcvd - sent;
+            
+            if (error) {
+                if (error instanceof ping.RequestTimedOutError) {
+                    io.emit('ping', index, false, 'timeout');
+                    stateChange(false, index, config.hosts[index].targetName, 'is disconnected' );
+                } else {
+                    io.emit('ping', index, false, 'error');
+                    stateChange(false, index, config.hosts[index].targetName, '(error)') ;
+                }
+            } else {
+                io.emit('ping', index, true, ms+'ms');
+                stateChange(true, index, config.hosts[index].targetName, 'is now UP (' + ms + 'ms)');
+            }
+        });
+    }
+},config.pingInterval);
+
+
+app.get('/', function(req, res){ 
+        res.render(__dirname + '/index', {
+            config: config,
+            logs: loadLogs()
+          });
+    });
+
+http.listen(80, function(){
+   console.log('App listening on port 3000');
+});
+
---
+++ public/client.js
@@ -0,0 +1,151 @@
+var socket = io();
+
+var hostsElement = document.getElementById('hosts');
+var messagesElement = document.getElementById('messages');
+var graphsElement = document.getElementById('graphs');
+
+var serverConfig = {};
+var firstConnect = true;
+
+var configModalEl = document.getElementById('configModal');
+var configButtonEl = document.getElementById('saveConfig');
+var configExButtonEl = document.getElementById('exampleConfig');
+var connectionModal = new bootstrap.Modal(document.getElementById('connectionModal'));
+var configModal = new bootstrap.Modal(document.getElementById('configModal'));
+var alertPlaceholder = document.getElementById('configAlertPlaceholder');
+
+
+
+
+
+
+function validateJson(json) {
+    try {
+        JSON.parse(json);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+
+function setStateUp(index, time) {
+    var div = document.getElementById('host_'+index);
+    div.children[0].className = 'oi oi-circle-check i-host i-host-up';
+    div.children[2].textContent = ' ('+time+')';
+}
+
+function setStateDown(index, time) {
+    var div = document.getElementById('host_'+index);
+    div.children[0].className = 'oi oi-circle-x i-host i-host-down';
+    div.children[2].textContent = ' ('+time+')';
+}
+
+
+function setLostConnection() {
+    var hosts = document.getElementsByName('hosts');
+    hosts.forEach(function(div) { 
+        div.children[0].className = 'oi oi-loop-circular i-host';
+        div.children[2].textContent = '';
+    });
+}
+
+
+function writeMessage(msg) {
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+    td.textContent = msg;
+    tr.appendChild(td);
+    messagesElement.prepend(tr);
+
+    if (messagesElement.childElementCount > 600) {
+        messagesElement.removeChild(messages.lastElementChild);
+    }
+}
+
+
+configModalEl.addEventListener('show.bs.modal', function (event) {
+    var pretty = JSON.stringify(serverConfig, undefined, 2);
+    document.getElementById('configTextarea').value = pretty;
+});
+
+
+configModalEl.addEventListener('hide.bs.modal', function (event) {
+    alertPlaceholder.innerHTML = '';
+});
+
+
+configExButtonEl.addEventListener('click', function (event) {
+    var exampleConfigJSON = {
+        "hosts": [
+            {
+                "targetName": "Offline",
+                "targetAddress": "8.8.8.8"
+            }
+        ],
+        "pingInterval": 5000,
+        "libreNMSURL": "http://10.200.202.26",
+        "libreNMSUsername": "username",
+        "libreNMSPassword": "password",
+        "sonicwallDeviceID": "2",
+        "graphs": [
+            {
+                "graphName": "DO Laptop",
+                "graphID": "12"
+            }
+        ]
+    };
+    var prettyJSON = JSON.stringify(exampleConfigJSON, undefined, 2);
+    document.getElementById('configTextarea').value = prettyJSON;
+});
+
+
+configButtonEl.addEventListener('click', function (event) {
+    var newConfig = document.getElementById('configTextarea').value;
+    if(validateJson(newConfig)) {
+        socket.emit('upstreamConfig', newConfig);
+        configModal.hide();
+    } else {
+        var wrapper = document.createElement('div');
+        var errorMessage = 'Error with config file. Check if file is in JSON format.';
+        wrapper.innerHTML = '<div class="alert alert-danger alert-dismissible" role="alert">'+errorMessage+'<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+        alertPlaceholder.append(wrapper);
+    }
+});
+
+
+socket.on('connect', function() {
+    connectionModal.hide();
+    if (firstConnect) {
+        firstConnect = false;
+    } else {
+        //socket.emit('serverReconnect');
+        window.location.reload();
+    }
+});
+
+
+socket.on('downstreamConfig', function(config) {
+    serverConfig = config;
+});
+
+
+socket.on('connect_error', function() {
+    connectionModal.show();
+    setLostConnection();
+});
+
+
+socket.on('message', function(msg) {
+    writeMessage(msg);
+});
+
+
+socket.on('ping', function(index, upState, time) {
+    if (upState) {
+        setStateUp(index, time);
+    } else {
+        setStateDown(index, time);
+    }
+});
+
